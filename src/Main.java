3c7b42edc5c6b764fbc6c177843c0bffbd1aa3d4
Sugrobkin <dsugrobkin148@gmail.com>
2023-04-02 17:43:22 +0300

    miles

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int price = 13200;
+
+        int miles = price / 20;
+        System.out.printf("Бонус: " + miles + " миль");
+
     }
 }
